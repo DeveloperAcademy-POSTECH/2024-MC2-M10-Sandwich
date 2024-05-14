17d6b44e7ae10abc4525df1172f5a1dc61e63c9d
thinkyside <eunlyuing@gmail.com>
2024-05-14 15:06:11 +0900

    [#1] 데이터 뷰 적용 예시 추가

--- MC2-OneShot/Model/OneShotData.swift
+++ MC2-OneShot/Model/OneShotData.swift
@@ -9,30 +9,63 @@ import Foundation
 // import SwiftData
 
 // @Model
-struct Party {
+class Party: Identifiable {
     let id: UUID = .init()
     
     let title: String
     let startDate: Date
     let notiCycle: Int
-    var stepList: [Step] = []
+    var stepList: [Step]
     
-    var isLive: Bool = true
-    var isShutdown: Bool = false
+    var isLive: Bool
+    var isShutdown: Bool
     
     let memberList: [Member]?
     var comment: String?
+    
+    init(
+        title: String,
+        startDate: Date,
+        notiCycle: Int,
+        stepList: [Step] = [],
+        isLive: Bool = true,
+        isShutdown: Bool = false,
+        memberList: [Member]? = nil,
+        comment: String? = nil
+    ) {
+        self.title = title
+        self.startDate = startDate
+        self.notiCycle = notiCycle
+        self.stepList = stepList
+        self.isLive = isLive
+        self.isShutdown = isShutdown
+        self.memberList = memberList
+        self.comment = comment
+    }
 }
 
-struct Step {
+class Step {
     var mediaList: [Media]
+    
+    init(mediaList: [Media]) {
+        self.mediaList = mediaList
+    }
 }
 
-struct Media {
+class Media {
     var fileData: String // TODO: 어떤 타입이 들어갈 수 있는지 리서치
     var captureDate: Date
+    
+    init(fileData: String, captureDate: Date) {
+        self.fileData = fileData
+        self.captureDate = captureDate
+    }
 }
 
-struct Member {
+class Member {
     let profileImage: String // TODO: 어떤 타입이 들어갈 수 있는지 리서치
+    
+    init(profileImage: String) {
+        self.profileImage = profileImage
+    }
 }
--- MC2-OneShot/Presentation/View/HomeView.swift
+++ MC2-OneShot/Presentation/View/HomeView.swift
@@ -11,17 +11,30 @@ struct HomeView: View {
     
     @StateObject private var pathModel: PathModel = .init()
     
+    // 1. 더미데이터 예시
+    // 요렇게 데이터를 만들어서 뷰에서 가져다 쓰세요
+//    let partys: [Party] = [
+//        Party(
+//            title: "안녕하세요",
+//            startDate: Date(),
+//            notiCycle: 30
+//        )
+//    ]
+    
     var body: some View {
         NavigationStack(path: $pathModel.paths) {
+//            List(partys) { party in
+//                Text("\(party.title)")
+//            }
             Text("HomeView")
-                .navigationDestination(for: PathType.self) { path in
-                    switch path {
-                    case .partySet: PartySetView()
-                    case .partyCamera: PartyCameraView()
-                    case .partyList: PartyListView()
-                    case .partyResult: PartyResultView()
-                    }
+            .navigationDestination(for: PathType.self) { path in
+                switch path {
+                case .partySet: PartySetView()
+                case .partyCamera: PartyCameraView()
+                case .partyList: PartyListView()
+                case .partyResult: PartyResultView()
                 }
+            }
         }
         .environmentObject(pathModel)
     }
