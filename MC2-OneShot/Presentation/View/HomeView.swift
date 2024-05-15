fc0dbaef026f11c870c298b32e63ee9b2824a281
thinkyside <eunlyuing@gmail.com>
2024-05-15 15:32:23 +0900

    [#8] ActionButton 컴포넌트 구현

--- MC2-OneShot/Presentation/View/HomeView.swift
+++ MC2-OneShot/Presentation/View/HomeView.swift
@@ -11,34 +11,22 @@ struct HomeView: View {
     
     @StateObject private var pathModel: PathModel = .init()
     
-    // 1. 더미데이터 예시
-    // 요렇게 데이터를 만들어서 뷰에서 가져다 쓰세요
-//    let partys: [Party] = [
-//        Party(
-//            title: "안녕하세요",
-//            startDate: Date(),
-//            notiCycle: 30
-//        )
-//    ]
-    
     var body: some View {
         NavigationStack(path: $pathModel.paths) {
-//            List(partys) { party in
-//                Text("\(party.title)")
-//            }
             Text("HomeView")
-                 .pretendard(.extraBold, 30)
-                 .foregroundStyle(.shot1C)
-            .navigationDestination(for: PathType.self) { path in
-                switch path {
-                case .partySet: PartySetView()
-                case .partyCamera: PartyCameraView()
-                case .partyList: PartyListView()
-                case .partyResult: PartyResultView()
+                .pretendard(.extraBold, 30)
+                .foregroundStyle(.shot1C)
+                .navigationDestination(for: PathType.self) { path in
+                    switch path {
+                    case .partySet: PartySetView()
+                    case .partyCamera: PartyCameraView()
+                    case .partyList: PartyListView()
+                    case .partyResult: PartyResultView()
+                    }
                 }
-            }
         }
         .environmentObject(pathModel)
+        .background(Color.shot00)
     }
 }
 
