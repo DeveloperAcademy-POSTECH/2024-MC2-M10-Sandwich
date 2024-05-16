862cce593246bc7f1f206344314f5d850cb1c309
thinkyside <eunlyuing@gmail.com>
2024-05-16 20:29:01 +0900

    [#32] PartySetView 코드 정리

--- MC2-OneShot/Presentation/View/PartySetView.swift
+++ MC2-OneShot/Presentation/View/PartySetView.swift
@@ -12,36 +12,8 @@ struct PartySetView: View {
     @EnvironmentObject private var pathModel: PathModel
     
     @State private var titleText: String = ""
-    @State private var notiCycle = 30
     
     @Binding var isPartySetViewPresented: Bool
-    
-    enum MemberForm: Hashable {
-        case member(name: String)
-        case add
-    }
-    
-    let memberForms: [MemberForm] = [
-        .member(name: "김민준"),
-        .member(name: "오띵진"),
-        .member(name: "정혜정"),
-        .member(name: "김유빈"),
-        .member(name: "장종현"),
-        .member(name: "김여운"),
-    ]
-    
-    let columns: [GridItem] = [
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
-    
-    var memberButtons: [MemberForm] {
-        var memberArray = memberForms
-        memberArray.append(.add)
-        return memberArray
-    }
     
     var body: some View {
         NavigationView {
@@ -54,80 +26,9 @@ struct PartySetView: View {
                     .foregroundStyle(.shotFF)
                 
                 List {
-                    
-                    // MARK: - 제목 입력
                     TextField("제목", text: $titleText)
-                    
-                    // MARK: - 사람 추가
-                    Section {
-                        VStack(alignment: .leading) {
-                            Text("사람 추가")
-                                .pretendard(.regular, 17)
-                                .foregroundStyle(.shotFF)
-                                .padding(.top, 6)
-                            
-                            Spacer()
-                                .frame(height: 16)
-                            
-                            LazyVGrid(columns: columns, spacing: 30) {
-                                ForEach(memberButtons, id: \.self) { member in
-                                    if member == .add {
-                                        Button {
-                                         //카메라 연결
-                                        } label: {
-                                            ZStack{
-
-                                                Circle()
-                                                    .frame (width: 60)
-                                                    .foregroundStyle(.shot33)
-                                                Image(systemName: "plus")
-                                                    .resizable()
-                                                    .frame(width: 32, height: 32)
-                                                    .foregroundStyle(.blue)
-                                            }
-                                        }
-                                        
-                                    } else {
-                                        Image(.test)
-                                            .resizable()
-                                            .frame (width: 60, height: 60)
-                                            .clipShape(Circle())
-                                    }
-                                }
-                            }
-                            .padding(.bottom, 8)
-                        }
-                    } footer: {
-                        Text("술자리를 함께하는 일행의 사진을 찍어봐요")
-                            .pretendard(.regular, 14)
-                    }
-                    
-                    // MARK: - 알람 주기
-                    Section {
-                        HStack {
-                            Text("알람 주기")
-                            
-                            Spacer()
-                            
-                            Menu {
-                                Button("30분") { notiCycle = 30 }
-                                Button("60분") { notiCycle = 60 }
-                                Button("90분") {notiCycle = 90 }
-                                Button("120분") { notiCycle = 120 }
-                            } label: {
-                                HStack {
-                                    Text("\(notiCycle)분")
-                                        .pretendard(.regular, 17)
-                                    
-                                    Image(systemName: "chevron.up.chevron.down")
-                                }
-                                .foregroundStyle(.shotFF).opacity(0.6)
-                            }
-                        }
-                    } footer: {
-                        Text("알림 주기마다 PUSH 알림을 보내드려요")
-                            .pretendard(.regular, 14)
-                    }
+                    MemberListView()
+                    NotiCycleView()
                 }
                 
                 Spacer()
@@ -144,6 +45,123 @@ struct PartySetView: View {
                 }
                 .padding(16)
             }
+        }
+    }
+}
+
+// MARK: - MemberListView
+private struct MemberListView: View {
+    
+    /// 멤버 버튼 타입 정의를 위한 열거형
+    enum MemberForm: Hashable {
+        case member(name: String)
+        case add
+    }
+    
+    /// 멤버 더미데이터
+    let memberForms: [MemberForm] = [
+        .member(name: "김민준"),
+        .member(name: "오띵진"),
+        .member(name: "정혜정"),
+        .member(name: "김유빈"),
+        .member(name: "장종현"),
+        .member(name: "김여운"),
+    ]
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    /// 멤버 버튼 생성을 위한 계산 속성
+    var memberButtons: [MemberForm] {
+        var memberArray = memberForms
+        memberArray.append(.add)
+        return memberArray
+    }
+    
+    var body: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Text("사람 추가")
+                    .pretendard(.regular, 17)
+                    .foregroundStyle(.shotFF)
+                    .padding(.top, 6)
+                
+                Spacer()
+                    .frame(height: 16)
+                
+                LazyVGrid(columns: columns, spacing: 30) {
+                    ForEach(memberButtons, id: \.self) { member in
+                        
+                        // 버튼이 Add일경우
+                        if member == .add {
+                            Button {
+                                //카메라 연결
+                            } label: {
+                                ZStack{
+                                    
+                                    Circle()
+                                        .frame (width: 60)
+                                        .foregroundStyle(.shot33)
+                                    Image(systemName: "plus")
+                                        .resizable()
+                                        .frame(width: 32, height: 32)
+                                        .foregroundStyle(.blue)
+                                }
+                            }
+                        }
+                        
+                        // 멤버 이미지일 경우
+                        else {
+                            Image(.test)
+                                .resizable()
+                                .frame (width: 60, height: 60)
+                                .clipShape(Circle())
+                        }
+                    }
+                }
+                .padding(.bottom, 8)
+            }
+        } footer: {
+            Text("술자리를 함께하는 일행의 사진을 찍어봐요")
+                .pretendard(.regular, 14)
+        }
+    }
+}
+
+// MARK: - NotiCycleView
+private struct NotiCycleView: View {
+    
+    @State private var notiCycle = 30
+    
+    var body: some View {
+        Section {
+            HStack {
+                Text("알람 주기")
+                
+                Spacer()
+                
+                Menu {
+                    Button("30분") { notiCycle = 30 }
+                    Button("60분") { notiCycle = 60 }
+                    Button("90분") {notiCycle = 90 }
+                    Button("120분") { notiCycle = 120 }
+                } label: {
+                    HStack {
+                        Text("\(notiCycle)분")
+                            .pretendard(.regular, 17)
+                        
+                        Image(systemName: "chevron.up.chevron.down")
+                    }
+                    .foregroundStyle(.shotFF).opacity(0.6)
+                }
+            }
+        } footer: {
+            Text("알림 주기마다 PUSH 알림을 보내드려요")
+                .pretendard(.regular, 14)
         }
     }
 }
