30596ee4a796bfe900045239ce723f7d4cd7cf8b
DecideJung1221 <jung991221@naver.com>
2024-05-16 09:49:15 +0900

    [#15] DummyData notiCycle, isLive 수정

--- MC2-OneShot/Model/DummyData.swift
+++ MC2-OneShot/Model/DummyData.swift
@@ -11,7 +11,7 @@ let dummyPartys: [Party] = [
     Party(
         title: "Birthday Bash",
         startDate: Date(),
-        notiCycle: 7,
+        notiCycle: 30,
         stepList: [Step(mediaList: [Media(fileData: "photo1.jpg", captureDate: Date()), Media(fileData: "photo2.jpg", captureDate: Date())])],
         isLive: true,
         isShutdown: false,
@@ -21,7 +21,7 @@ let dummyPartys: [Party] = [
     Party(
         title: "Office Party",
         startDate: Date(),
-        notiCycle: 3,
+        notiCycle: 60,
         stepList: [Step(mediaList: [Media(fileData: "photo3.jpg", captureDate: Date()), Media(fileData: "photo4.jpg", captureDate: Date())])],
         isLive: false,
         isShutdown: true,
@@ -31,9 +31,9 @@ let dummyPartys: [Party] = [
     Party(
         title: "New Year Celebration",
         startDate: Date(),
-        notiCycle: 10,
+        notiCycle: 90,
         stepList: [Step(mediaList: [Media(fileData: "photo5.jpg", captureDate: Date()), Media(fileData: "photo6.jpg", captureDate: Date())])],
-        isLive: true,
+        isLive: false,
         isShutdown: false,
         memberList: [Member(profileImage: "member5.jpg"), Member(profileImage: "member6.jpg")],
         comment: "Happy New Year!"
@@ -41,9 +41,9 @@ let dummyPartys: [Party] = [
     Party(
         title: "Summer BBQ",
         startDate: Date(),
-        notiCycle: 14,
+        notiCycle: 120,
         stepList: [Step(mediaList: [Media(fileData: "photo7.jpg", captureDate: Date()), Media(fileData: "photo8.jpg", captureDate: Date())])],
-        isLive: true,
+        isLive: false,
         isShutdown: false,
         memberList: [Member(profileImage: "member7.jpg"), Member(profileImage: "member8.jpg")],
         comment: "Can't wait for the BBQ!"
@@ -51,9 +51,9 @@ let dummyPartys: [Party] = [
     Party(
         title: "Halloween Party",
         startDate: Date(),
-        notiCycle: 5,
+        notiCycle: 30,
         stepList: [Step(mediaList: [Media(fileData: "photo9.jpg", captureDate: Date()), Media(fileData: "photo10.jpg", captureDate: Date())])],
-        isLive: true,
+        isLive: false,
         isShutdown: false,
         memberList: [Member(profileImage: "member9.jpg"), Member(profileImage: "member10.jpg")],
         comment: "Spooky!"
@@ -61,7 +61,7 @@ let dummyPartys: [Party] = [
     Party(
         title: "Christmas Party",
         startDate: Date(),
-        notiCycle: 25,
+        notiCycle: 60,
         stepList: [Step(mediaList: [Media(fileData: "photo11.jpg", captureDate: Date()), Media(fileData: "photo12.jpg", captureDate: Date())])],
         isLive: false,
         isShutdown: true,
@@ -71,9 +71,9 @@ let dummyPartys: [Party] = [
     Party(
         title: "Graduation Party",
         startDate: Date(),
-        notiCycle: 12,
+        notiCycle: 90,
         stepList: [Step(mediaList: [Media(fileData: "photo13.jpg", captureDate: Date()), Media(fileData: "photo14.jpg", captureDate: Date())])],
-        isLive: true,
+        isLive: false,
         isShutdown: false,
         memberList: [Member(profileImage: "member13.jpg"), Member(profileImage: "member14.jpg")],
         comment: "Congratulations to all graduates!"
@@ -83,7 +83,7 @@ let dummyPartys: [Party] = [
         startDate: Date(),
         notiCycle: 30,
         stepList: [Step(mediaList: [Media(fileData: "photo15.jpg", captureDate: Date()), Media(fileData: "photo16.jpg", captureDate: Date())])],
-        isLive: true,
+        isLive: false,
         isShutdown: false,
         memberList: [Member(profileImage: "member15.jpg"), Member(profileImage: "member16.jpg")],
         comment: "Happy Anniversary!"
@@ -91,7 +91,7 @@ let dummyPartys: [Party] = [
     Party(
         title: "Farewell Party",
         startDate: Date(),
-        notiCycle: 8,
+        notiCycle: 120,
         stepList: [Step(mediaList: [Media(fileData: "photo17.jpg", captureDate: Date()), Media(fileData: "photo18.jpg", captureDate: Date())])],
         isLive: false,
         isShutdown: true,
@@ -101,9 +101,9 @@ let dummyPartys: [Party] = [
     Party(
         title: "Welcome Party",
         startDate: Date(),
-        notiCycle: 6,
+        notiCycle: 60,
         stepList: [Step(mediaList: [Media(fileData: "photo19.jpg", captureDate: Date()), Media(fileData: "photo20.jpg", captureDate: Date())])],
-        isLive: true,
+        isLive: false,
         isShutdown: false,
         memberList: [Member(profileImage: "member19.jpg"), Member(profileImage: "member20.jpg")],
         comment: "Welcome to the team!"
