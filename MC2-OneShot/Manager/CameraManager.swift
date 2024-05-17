3f68112496ad07dcdbac5fa5b26fdc5991913666
DecideJung1221 <jung991221@naver.com>
2024-05-17 10:39:09 +0900

    [#24] camera 촬영버튼 눌렀을 때, 구성전환

--- MC2-OneShot/Manager/CameraManager.swift
+++ MC2-OneShot/Manager/CameraManager.swift
@@ -4,5 +4,106 @@
 //
 //  Created by 정혜정 on 5/17/24.
 //
+import SwiftUI
+import AVFoundation
 
-import Foundation
+// NSObject 추가
+class CameraManager: NSObject, ObservableObject {
+    var session = AVCaptureSession()
+    var videoDeviceInput: AVCaptureDeviceInput!
+    let output = AVCapturePhotoOutput()
+    var photoData = Data(count: 0)
+    
+    @Published var recentImage: UIImage? // 추가
+    
+    // 카메라 셋업 과정을 담당하는 함수,
+    func setUpCamera() {
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                for: .video, position: .back) {
+            do { // 카메라가 사용 가능하면 세션에 input과 output을 연결
+                videoDeviceInput = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(videoDeviceInput) {
+                    session.addInput(videoDeviceInput)
+                }
+                
+                if session.canAddOutput(output) {
+                    session.addOutput(output)
+                    output.isHighResolutionCaptureEnabled = true
+                    output.maxPhotoQualityPrioritization = .quality
+                }
+                session.startRunning() // 세션 시작
+            } catch {
+                print(error) // 에러 프린트
+            }
+        }
+    }
+    
+    func requestAndCheckPermissions() {
+        // 카메라 권한 상태 확인
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            // 권한 요청
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] authStatus in
+                if authStatus {
+                    DispatchQueue.main.async {
+                        self?.setUpCamera()
+                    }
+                }
+            }
+        case .restricted:
+            break
+        case .authorized:
+            // 이미 권한 받은 경우 셋업
+            setUpCamera()
+        default:
+            // 거절했을 경우
+            print("Permession declined")
+        }
+    }
+    
+    func capturePhoto() {
+        // 사진 옵션 세팅
+        let photoSettings = AVCapturePhotoSettings()
+        
+        // 캡처 해상도 및 비율을 1:1로 설정
+        photoSettings.previewPhotoFormat = [
+            kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA),
+            kCVPixelBufferWidthKey as String: 1000,
+            kCVPixelBufferHeightKey as String: 1000
+        ]
+        
+        self.output.capturePhoto(with: photoSettings, delegate: self)
+        print("[Camera]: Photo's taken")
+    }
+    
+//    func savePhoto(_ imageData: Data) {
+//        guard let image = UIImage(data: imageData) else { return }
+//        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+//
+//        // 사진 저장하기
+//        print("[Camera]: Photo's saved")
+//    }
+}
+
+extension CameraManager: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, willBeginCaptureFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, willCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        self.recentImage = UIImage(data: imageData) // 추가
+//        self.savePhoto(imageData)
+        
+//        DispatchQueue.global(qos: .background).async{
+//            self.session.stopRunning() // 화면멈춤
+//        }
+        
+        print("[CameraModel]: Capture routine's done")
+    }
+}
--- MC2-OneShot/Manager/CameraViewManager.swift
+++ MC2-OneShot/Manager/CameraViewManager.swift
@@ -5,4 +5,75 @@
 //  Created by 정혜정 on 5/17/24.
 //
 
-import Foundation
+import SwiftUI
+import AVFoundation
+import Combine
+
+class CameraViewManager: ObservableObject {
+    private let model: CameraManager
+    private let session: AVCaptureSession
+    private var subscriptions = Set<AnyCancellable>() // 추가
+    let cameraPreview: AnyView
+
+    @Published var recentImage: UIImage? // 추가
+    @Published var isFlashOn = false
+    @Published var isSilentModeOn = false
+    
+    // 초기 세팅
+    func configure() {
+        model.requestAndCheckPermissions()
+    }
+    
+    // 플래시 온오프
+    func switchFlash() {
+        isFlashOn.toggle()
+    }
+    
+    // 사진 촬영
+    func capturePhoto() {
+        model.capturePhoto()
+        print("[CameraViewModel]: Photo captured!")
+    }
+    
+    // 전후면 카메라 스위칭
+    func changeCamera() {
+        print("[CameraViewModel]: Camera changed!")
+    }
+    
+    init() {
+        model = CameraManager()
+        session = model.session
+        cameraPreview = AnyView(CameraPreviewView(session: session))
+        
+        model.$recentImage.sink { [weak self] (photo) in // 추가
+            guard let pic = photo else { return }
+            // 이미지를 정사각형 모양으로 가져옴
+            if let croppedImage = self?.cropImageToSquare(image: pic) {
+                self?.recentImage = croppedImage
+            }
+        }
+        .store(in: &self.subscriptions)
+    }
+    
+    // 이미지를 정사각형 모양으로 자르는 함수
+    private func cropImageToSquare(image: UIImage) -> UIImage {
+        let cgImage = image.cgImage!
+        let width = CGFloat(cgImage.width)
+        let height = CGFloat(cgImage.height)
+        
+        let aspectRatio = width / height
+        var rect = CGRect.zero
+        
+        if aspectRatio > 1 { // 가로가 더 긴 경우
+            rect = CGRect(x: (width - height) / 2, y: 0, width: height, height: height)
+        } else { // 세로가 더 긴 경우
+            rect = CGRect(x: 0, y: (height - width) / 2, width: width, height: width)
+        }
+        
+        if let croppedCGImage = cgImage.cropping(to: rect) {
+            return UIImage(cgImage: croppedCGImage, scale: image.scale, orientation: image.imageOrientation)
+        }
+        
+        return image
+    }
+}
