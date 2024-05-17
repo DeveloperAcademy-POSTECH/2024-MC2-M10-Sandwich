0d12b764e7279e22ed292fb07d984b2d3c93c8b4
YuBin Kim <yubin412@naver.com>
2024-05-17 11:00:13 +0900

    [#36] Local Push Notification 기본 기능 구현

---
+++ MC2-OneShot/Manager/NotificationManager.swift
@@ -0,0 +1,83 @@
+//
+//  NotificationManager.swift
+//  MC2-OneShot
+//
+//  Created by KimYuBin on 5/17/24.
+//
+
+import SwiftUI
+import UserNotifications
+import CoreLocation
+
+struct LocalPushNotification: View {
+    let manager = NotificationManager.instance
+    var body: some View {
+        VStack(spacing: 40) {
+            Button("알림 권한 설정") {
+                manager.requestAuthorization()
+            }
+            Button("알림 예약") {
+                manager.scheduleNotification(trigger: .time)
+            }
+            Button("삭제") {
+                manager.cancelNotification()
+            }
+        }
+        .onAppear {
+            UNUserNotificationCenter.current().setBadgeCount(0, withCompletionHandler: nil)
+        }
+    }
+}
+
+class NotificationManager {
+    static let instance = NotificationManager()
+    private init() {}
+    
+    // 알림 설정 허용 여부
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { (success, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("SUCCESS")
+            }
+        }
+    }
+    
+    enum TriggerType: String {
+        case time = "time"
+        
+        var trigger: UNNotificationTrigger {
+            switch self {
+            case .time:
+                return UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false) // 알림 주기 입력
+            }
+        }
+    }
+    
+    func scheduleNotification(trigger: TriggerType) {
+        let content = UNMutableNotificationContent()
+        content.title = "알림 주기가 다가왔습니다"
+        content.subtitle = "사진을 찍어주세요!"
+        
+        let soundName = "customSound"
+        let soundExtension = "mp3"
+        
+        if let soundURL = Bundle.main.url(forResource: soundName, withExtension: soundExtension) {
+            let sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: soundName + "." + soundExtension))
+            content.sound = sound
+        } else {
+            print("사운드 파일을 찾을 수 없습니다.")
+        }
+
+        content.badge = 1
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger.trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
+    
+    func cancelNotification() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests() // 삭제
+    }
+}
