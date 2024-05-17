63668ee2032d9517d0713bc8d1caff557247aee9
DecideJung1221 <jung991221@naver.com>
2024-05-17 09:52:18 +0900

    [#24] camera 화면 구현, manager 세팅 info.plist 추가

---
+++ MC2-OneShot/Manager/CameraManager.swift
@@ -0,0 +1,8 @@
+//
+//  CameraManager.swift
+//  MC2-OneShot
+//
+//  Created by 정혜정 on 5/17/24.
+//
+
+import Foundation
---
+++ MC2-OneShot/Manager/CameraViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  CameraViewManager.swift
+//  MC2-OneShot
+//
+//  Created by 정혜정 on 5/17/24.
+//
+
+import Foundation
