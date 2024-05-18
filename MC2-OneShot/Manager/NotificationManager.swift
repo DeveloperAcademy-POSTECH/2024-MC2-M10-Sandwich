a2646450a377d73e792d1c4946b5dc1820bc1096
jongchang <hokhmah91@naver.com>
2024-05-18 15:50:47 +0900

    [#36] 인자타입 Date로 변환

--- MC2-OneShot/Manager/NotificationManager.swift
+++ MC2-OneShot/Manager/NotificationManager.swift
@@ -10,6 +10,9 @@ import UserNotifications
 import CoreLocation
 
 struct LocalPushNotification: View {
+    
+    let dummyDate = Date() + 15
+    
     let manager = NotificationManager.instance
     var body: some View {
         VStack(spacing: 40) {
@@ -17,7 +20,9 @@ struct LocalPushNotification: View {
                 manager.requestAuthorization()
             }
             Button("알림 예약") {
-                manager.scheduleNotification(trigger: .calender)
+                print("now: ", Date())
+                print("alarmTime: ", dummyDate)
+                manager.scheduleNotification(date: dummyDate)
             }
             Button("삭제") {
                 manager.cancelNotification()
@@ -45,19 +50,19 @@ class NotificationManager {
         }
     }
     
-    enum TriggerType: String {
-        case calender = "calender"
-        
-        var trigger: UNNotificationTrigger {
-            switch self {
-            case .calender:
-                let dateComponent = DateComponents(year: 2024, month: 5, day: 17, hour: 15, minute: 43)
-                return UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
-            }
-        }
-    }
+    //    enum TriggerType: String {
+    //        case calender = "calender"
+    //
+    //        var trigger: UNNotificationTrigger {
+    //            switch self {
+    //            case .calender:
+    //                let dateComponent = DateComponents(year: 2024, month: 5, day: 17, hour: 15, minute: 43)
+    //                return UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+    //            }
+    //        }
+    //    }
     
-    func scheduleNotification(trigger: TriggerType) {
+    func scheduleNotification(date: Date) {
         
         let content = UNMutableNotificationContent()
         content.title = "알림 주기가 다가왔습니다"
@@ -75,7 +80,17 @@ class NotificationManager {
         
         content.badge = 1
         
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger.trigger)
+        var calendar = Calendar.current
+        calendar.locale = Locale(identifier: "ko_KR")
+        
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        print("type: ", type(of: dateComponents))
+        print("dateComponents: ", dateComponents)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         UNUserNotificationCenter.current().add(request)
         
     }
