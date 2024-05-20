e4b7f831e8791cfc6f801786ee6053e59c552a31
YuBin Kim <yubin412@naver.com>
2024-05-20 12:25:16 +0900

    [#50] 무음모드, 앱 켜진 상태 -> 소리 추가

--- MC2-OneShot/App/AppDelegate.swift
+++ MC2-OneShot/App/AppDelegate.swift
@@ -6,19 +6,50 @@
 //
 
 import SwiftUI
+import UserNotifications
+import AVFoundation
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         UNUserNotificationCenter.current().delegate = self
-        
         return true
     }
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    // Foreground(앱이 켜진 상태)에서도 알림이 오도록 설정
+    
+    // Foreground(앱이 켜진 상태)
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.list, .banner])
+        
+        print("알림 표시")
+        playSound()
+    }
+    
+    // 사용자가 알림을 탭하여 앱을 열 때 호출되는 메서드
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    var audioPlayer: AVAudioPlayer?
+    
+    private func playSound() {
+        let soundName = "CustomSound"
+        let soundExtension = "mp3"
+        
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: soundExtension) else {
+            print("사운드 파일을 찾을 수 없습니다.")
+            return
+        }
+        
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.playback, mode: .default, options: [.mixWithOthers])
+            try audioSession.setActive(true)
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+            
+            print("재생됨")
+        } catch {
+            print("Failed to play sound: \(error.localizedDescription)")
+        }
     }
 }
