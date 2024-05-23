f4df0c356df9a58e4018808f065835b68c2cae29
thinkyside <eunlyuing@gmail.com>
2024-05-23 09:54:04 +0900

    [#132] HomeView 버튼 텍스트 수정

--- MC2-OneShot/Presentation/View/HomeView.swift
+++ MC2-OneShot/Presentation/View/HomeView.swift
@@ -131,8 +131,7 @@ struct HomeView: View {
                 }
 
                 ActionButton(
-                                                                 // 내용 바꿈
-                    title: isCurrentPartyLive ? "술자리 돌아가기" : "사진찍으러 가기",
+                    title: isCurrentPartyLive ? "사진찍으러 가기" : "술자리 돌아가기",
                     buttonType: isCurrentPartyLive ? .popupfinish : .primary
                 ) {
                     if isCurrentPartyLive {
