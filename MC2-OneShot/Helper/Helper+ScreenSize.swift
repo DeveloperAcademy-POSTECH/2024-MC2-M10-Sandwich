15b6ea04163e6b4589dce077e4e8752291261b6a
DecideJung1221 <jung991221@naver.com>
2024-05-28 11:32:43 +0900

    [#180] 화면 width 크기 파일 생성

---
+++ MC2-OneShot/Helper/Helper+ScreenSize.swift
@@ -0,0 +1,15 @@
+//
+//  Helper+ScreenSize.swift
+//  MC2-OneShot
+//
+//  Created by 정혜정 on 5/28/24.
+//
+
+import Foundation
+
+enum ScreenSize{
+    
+    // 스크린의 width,heigh를 반환합니다.
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeigh = UIScreen.main.bounds.size.height
+}
